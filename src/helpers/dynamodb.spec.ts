53bb02b8ce33a8eeba3e7dc4f4c26fec535783be
mrutyunjay <mrutyunjay.swain@gmail.com>
2021-03-31 00:06:36 +0800

    updated mocha tests

--- src/helpers/dynamodb.spec.ts
+++ src/helpers/dynamodb.spec.ts
@@ -1,7 +1,44 @@
-import { expect } from 'chai';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { expect, use } from 'chai';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+import * as chaiSubset from 'chai-subset';
+use(sinonChai);
+use(chaiSubset);
+import { queryUsers } from './dynamodb';
 
 describe('dynamodb helper', () => {
+  let queryStub: sinon.SinonStub;
+
+  before(() => {
+    process.env.USER_TABLE = 'users';
+    queryStub = sinon.stub(DynamoDBDocumentClient.prototype, 'send');
+  });
+
+  afterEach(() => queryStub.reset());
+
   describe('queryUsers', () => {
-    expect(1).to.equal(1);
+    it('should query ddb and get list of users', async () => {
+      queryStub.resolves({ Items: [{ pk: 'mj', name: 'mswain', city: 'perth', country: 'australia' }] });
+      const response = await queryUsers('test user');
+      expect(response).to.deep.equal([{ name: 'mswain', city: 'perth', country: 'australia', email: 'mj' }]);
+    });
+
+    it('should query ddb with the required params', async () => {
+      queryStub.resolves({ Items: [] });
+      await queryUsers('test user');
+
+      expect(queryStub).to.have.been.calledWithMatch({
+        QueryCommand: {
+          input: {
+            TableName: 'users',
+            IndexName: 'sk-name',
+            KeyConditionExpression: 'sk = :pk AND begins_with(#user, :name)',
+            ExpressionAttributeNames: [Object],
+            ExpressionAttributeValues: [Object],
+          },
+        },
+      });
+    });
   });
 });
--- src/helpers/dynamodb.ts
+++ src/helpers/dynamodb.ts
@@ -9,22 +9,15 @@ let client = process.env.IS_OFFLINE
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
 export const queryUsers = async (name: string): Promise<User[] | []> => {
-  console.info(process.env.IS_OFFLINE, region);
   const queryCommand = new QueryCommand({
     TableName: process.env.USER_TABLE,
     IndexName: 'sk-name',
     KeyConditionExpression: 'sk = :pk AND begins_with(#user, :name)',
-    ExpressionAttributeNames: {
-      '#user': 'name',
-    },
-    ExpressionAttributeValues: {
-      ':pk': 'profile',
-      ':name': name,
-    },
+    ExpressionAttributeNames: { '#user': 'name' },
+    ExpressionAttributeValues: { ':pk': 'profile', ':name': name },
   });
 
   const { Items } = await ddbDocClient.send(queryCommand);
-  console.info(Items);
   const users: User[] = Items.map((item) => {
     const { name, city, country, pk: email } = item;
     return { name, city, country, email };
